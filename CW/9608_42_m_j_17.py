96d5015311c748403d3aed028f564a6cb11758cf
Justin He <justin.he814@gmail.com>
2025-02-11 15:26:46 +0200

    completed some other questions

--- CW/9608_42_m_j_17.py
+++ CW/9608_42_m_j_17.py
@@ -5,9 +5,19 @@ class IslandClass:
             grid.append(['.' for i in range(30)])
         self.Grid = grid
     def HideTreasure(self):
-        pass
+        import random
+        TREASURE = "T"
+        Row = random.randint(0,11)
+        Column = random.randint(0, 31)
+        self.Grid[Row][Column] = TREASURE
     def DigHole(self, Row, Column):
-        pass
+        HOLE = "X"
+        FOUND_HOLE = False
+        if self.Grid[Row][Column] == TREASURE:
+            self.Grid[Row][Column] = HOLE
+            FOUND_HOLE = True
+        else:
+            self.Grid[Row][Column] = HOLE
     def GetSquare(self, Row, Column):
         return self.Grid[Row][Column]
     def DisplayGrid(self):
