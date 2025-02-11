91c1ae991358f0af9b851f44e483554a4679cbcc
Justin He <ate329@ate329.com>
2025-02-11 14:59:50 +0200

    update CW and HW

---
+++ CW/9608_42_m_j_17.py
@@ -0,0 +1,12 @@
+class IslandClass:
+    def __init__(self):
+        grid = []
+        for i in range(10):
+            grid.append(['.' for i in range(30)])
+        self.Grid = grid
+    def HideTreasure(self):
+        pass
+    def DigHole(Row, Column):
+        pass
+    def GetSquare(Row, Column):
+        pass
