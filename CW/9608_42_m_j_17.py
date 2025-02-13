073818a4412741d86a3587fad9c4be3c04df8726
Justin He <justin.he814@gmail.com>
2025-02-13 09:28:55 +0200

    format

--- CW/9608_42_m_j_17.py
+++ CW/9608_42_m_j_17.py
@@ -4,12 +4,14 @@ class IslandClass:
         for i in range(10):
             grid.append(['.' for i in range(30)])
         self.Grid = grid
+
     def HideTreasure(self):
         import random
         TREASURE = "T"
-        Row = random.randint(0,11)
+        Row = random.randint(0, 11)
         Column = random.randint(0, 31)
         self.Grid[Row][Column] = TREASURE
+
     def DigHole(self, Row, Column):
         HOLE = "X"
         FOUND_HOLE = False
@@ -18,8 +20,10 @@ class IslandClass:
             FOUND_HOLE = True
         else:
             self.Grid[Row][Column] = HOLE
+
     def GetSquare(self, Row, Column):
         return self.Grid[Row][Column]
+
     def DisplayGrid(self):
         for i in range(10):
             for j in range(30):
