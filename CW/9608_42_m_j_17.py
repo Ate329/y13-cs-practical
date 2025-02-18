4e88e56a91390b52c53fba7fa4c6c072e88c90e7
Ate329 <justin.he814@gmail.com>
2025-02-18 15:19:14 +0200

    optimise 9608_42_m_j_17 and add test cases

--- CW/9608_42_m_j_17.py
+++ CW/9608_42_m_j_17.py
@@ -1,31 +1,29 @@
 class IslandClass:
     def __init__(self):
-        grid = []
-        for i in range(10):
-            grid.append(['.' for i in range(30)])
-        self.Grid = grid
+        self.Grid = [['.' for _ in range(30)] for _ in range(10)]
 
     def HideTreasure(self):
         import random
         TREASURE = "T"
-        Row = random.randint(0, 11)
-        Column = random.randint(0, 31)
+        Row = random.randint(0, 9)  # Fixed range to match grid size
+        Column = random.randint(0, 29)  # Fixed range to match grid size
         self.Grid[Row][Column] = TREASURE
 
     def DigHole(self, Row, Column):
         HOLE = "X"
-        FOUND_HOLE = False
-        if self.Grid[Row][Column] == TREASURE:
-            self.Grid[Row][Column] = HOLE
-            FOUND_HOLE = True
-        else:
-            self.Grid[Row][Column] = HOLE
+        if 0 <= Row < 10 and 0 <= Column < 30:  # Added bounds checking
+            if self.Grid[Row][Column] == "T":  # Changed TREASURE to "T"
+                self.Grid[Row][Column] = HOLE
+                return True  # Return whether treasure was found
+            else:
+                self.Grid[Row][Column] = HOLE
+        return False
 
     def GetSquare(self, Row, Column):
-        return self.Grid[Row][Column]
+        if 0 <= Row < 10 and 0 <= Column < 30:  # Added bounds checking
+            return self.Grid[Row][Column]
+        return None  # Return None for invalid coordinates
 
     def DisplayGrid(self):
-        for i in range(10):
-            for j in range(30):
-                print(GetSquare(i, j), end='')
-            print()
+        for row in self.Grid:
+            print(''.join(row))  # Simplified grid display
