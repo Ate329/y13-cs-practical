c5a5e1e2ced33a62bd7f318cc4de8170a72afe5b
Ate329 <justin.he814@gmail.com>
2025-02-18 15:13:20 +0200

    complete 9608_41_m_j_18

---
+++ CW/9608_41_m_j_18.py
@@ -0,0 +1,37 @@
+# Q5B
+class GameElement:
+    def __init__(self, PositionX: int, PositionY: int,
+                 Width: int, Height: int, ImageFilename: str):
+        self.PositionX = PositionX
+        self.PositionY = PositionY
+        self.Width = Width
+        self.Height = Height
+        self.ImageFilename = ImageFilename
+
+    def GetDetails(self):
+        return f"Position: {self.PositionX}, {self.PositionY}, Size: {self.Width}x{self.Height}, Image: {self.ImageFilename}"
+
+# Q5C
+class Scenery(GameElement):
+    def __init__(self, PositionX: int, PositionY: int,
+                 Width: int, Height: int, ImageFilename: str,
+                 CauseDamage: bool, DamagePoints: int):
+        super().__init__(PositionX, PositionY, Width, Height, ImageFilename)
+        self.CauseDamage = CauseDamage
+        self.DamagePoints = DamagePoints if CauseDamage else 0
+    
+    def GiveDamagePoints(self):
+        return self.DamagePoints if self.CauseDamage else 0
+    
+    # Q5Dii
+    def GetScenery(self):
+        return f"Scenery: CauseDamage: {self.CauseDamage}, DamagePoints: {self.DamagePoints}, Details: {self.GetDetails()}"
+
+# Q5Di some funny stuff here
+class GiftBox(Scenery):
+    def __init__(self, PositionX: int = 150, PositionY: int = 150,
+                 Width: int = 50, Height: int = 75,
+                 ImageFilename: str = "box.png",
+                 CauseDamage: bool = True, DamagePoints: int = 50):
+        super().__init__(PositionX, PositionY, Width, Height, ImageFilename,
+                        CauseDamage, DamagePoints)
