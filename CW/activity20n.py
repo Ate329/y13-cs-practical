34b717056d8d69a80b57a561c37d7631cbbb00a3
Ate329 <justin.he814@gmail.com>
2025-03-11 14:42:12 +0200

    make output look better

--- CW/activity20n.py
+++ CW/activity20n.py
@@ -4,6 +4,8 @@ while True:
         print(f"You entered: {integer}")
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt \nProgram stopped")
+        break
     else:
         print("Thank you for entering a valid integer.")
-        break
