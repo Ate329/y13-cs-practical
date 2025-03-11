775ea7baf86c20e01ca554ec86087d2722f86f55
Ate329 <justin.he814@gmail.com>
2025-03-11 14:47:13 +0200

    better activity 20n

--- CW/activity20n.py
+++ CW/activity20n.py
@@ -1,11 +1,18 @@
-while True:
-    try:
-        integer = int(input("Enter an integer: "))
-        print(f"You entered: {integer}")
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
-    except KeyboardInterrupt:
-        print("\nKeyboardInterrupt \nProgram stopped")
-        break
-    else:
-        print("Thank you for entering a valid integer.")
+def test_try_exepct():
+    while True:
+        try:
+            integer = int(input("Enter an integer: "))
+            print(f"You entered: {integer}")
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+        except KeyboardInterrupt:
+            print("\nKeyboardInterrupt \nProgram stopped")
+            break
+        else:
+            print("Thank you for entering a valid integer.")
+
+def main():
+    test_try_exepct()
+
+if __name__ == "__main__":
+    main()
